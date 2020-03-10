3c08300eb4b4eee79df1e81ca110b892e8ee03f4
Wesley <cptwesley@jojoweba.nl>
2020-03-10 16:48:36 +0100

    Add some basic parser combinator stuff

---
+++ src/Warpstone/ParseResult.cs
@@ -0,0 +1,42 @@
+﻿namespace Warpstone
+{
+    /// <summary>
+    /// Object representing the parsing result.
+    /// </summary>
+    /// <typeparam name="T">The output type of the parse process.</typeparam>
+    public class ParseResult<T>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParseResult{T}"/> class.
+        /// </summary>
+        public ParseResult()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ParseResult{T}"/> class.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="position">The position of the parser.</param>
+        public ParseResult(T value, int position)
+        {
+            Value = value;
+            Position = position;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the parsing was success.
+        /// </summary>
+        public bool Success => Value != null;
+
+        /// <summary>
+        /// Gets the parsed value.
+        /// </summary>
+        public T Value { get; }
+
+        /// <summary>
+        /// Gets the position of the parser.
+        /// </summary>
+        public int Position { get; }
+    }
+}
---
+++ src/Warpstone/Parser.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Warpstone
+{
+    /// <summary>
+    /// Parser class for parsing textual input.
+    /// </summary>
+    /// <typeparam name="TOutput">The type of the output.</typeparam>
+    public abstract class Parser<TOutput>
+    {
+        public ParseResult<TOutput> Parse(string input)
+            => Parse(input, 0);
+
+        internal abstract ParseResult<TOutput> Parse(string input, int position);
+    }
+
+    public static class Parser
+    {
+        public static Parser<char> Char(char c)
+            => new CharParser(c);
+
+        public static Parser<IEnumerable<T>> Many<T>(Parser<T> parser)
+            => new ManyParser<T>(parser);
+
+        public static Parser<T> Or<T>(Parser<T> first, Parser<T> second)
+            => new OrParser<T>(first, second);
+    }
+
+    internal class CharParser : Parser<char>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CharParser"/> class.
+        /// </summary>
+        /// <param name="c">The character expecting in the parser.</param>
+        internal CharParser(char c)
+            => Character = c;
+
+        /// <summary>
+        /// Gets the expected character.
+        /// </summary>
+        internal char Character { get; }
+
+        /// <inheritdoc/>
+        internal override ParseResult<char> Parse(string input, int position)
+        {
+            if (input[position] == Character)
+            {
+                return new ParseResult<char>(input[position], position + 1);
+            }
+
+            return new ParseResult<char>();
+        }
+    }
+
+    internal class ManyParser<T> : Parser<IEnumerable<T>>
+    {
+        internal ManyParser(Parser<T> parser)
+            => Parser = parser;
+
+        internal Parser<T> Parser { get; }
+
+        /// <inheritdoc/>
+        internal override ParseResult<IEnumerable<T>> Parse(string input, int position)
+        {
+            List<T> elements = new List<T>();
+            int newPosition = position;
+            ParseResult<T> result;
+            while (true)
+            {
+                result = Parser.Parse(input, newPosition);
+                if (!result.Success)
+                {
+                    break;
+                }
+
+                newPosition = result.Position;
+                elements.Add(result.Value);
+            }
+
+            return new ParseResult<IEnumerable<T>>(elements, newPosition + 1);
+        }
+    }
+
+    internal class OrParser<T> : Parser<T>
+    {
+        internal OrParser(Parser<T> first, Parser<T> second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        internal Parser<T> First { get; }
+        internal Parser<T> Second { get; }
+
+        internal override ParseResult<T> Parse(string input, int position)
+        {
+            ParseResult<T> firstResult = First.Parse(input, position);
+            if (firstResult.Success)
+            {
+                return firstResult;
+            }
+
+            return Second.Parse(input, position);
+        }
+    }
+
+    internal class TransformParser<TInput, TOutput> : Parser<TOutput>
+    {
+        internal TransformParser(Parser<TInput> parser, Func<TInput, TOutput> transformation)
+        {
+            Parser = parser;
+            Transformation = transformation;
+        }
+
+        internal Parser<TInput> Parser { get; }
+
+        internal Func<TInput, TOutput> Transformation { get; }
+
+        internal override ParseResult<TOutput> Parse(string input, int position)
+        {
+            ParseResult<TInput> result = Parser.Parse(input, position);
+            if (result.Success)
+            {
+                return new ParseResult<TOutput>(Transformation(result.Value), result.Position);
+            }
+
+            return new ParseResult<TOutput>();
+        }
+    }
+}
