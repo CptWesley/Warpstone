5ab50a092869c30ece72178bd92e3b4d4e9b5be6
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2020-03-13 22:48:37 +0100

    no message

--- src/Warpstone.Examples.Expressions/ExpressionParser.cs
+++ src/Warpstone.Examples.Expressions/ExpressionParser.cs
@@ -7,24 +7,6 @@ namespace Warpstone.Examples.Expressions
         private static readonly Parser<Expression> Num
             = Regex("[0-9]+").Transform(x => new NumExpression(int.Parse(x)) as Expression).Trim();
 
-        /*
-        private static readonly Parser<Expression> Add
-            = Or(
-                Lazy(() => Num).ThenSkip(Char('+')).ThenAdd(Lazy(() => Exp))
-                    .Transform(x => new AddExpression(x.Item1, x.Item2) as Expression),
-                Num);
-
-        private static readonly Parser<Expression> Mul
-            = Or(
-                Lazy(() => Add).ThenSkip(Char('*')).ThenAdd(Lazy(() => Exp))
-                    .Transform(x => new MulExpression(x.Item1, x.Item2) as Expression),
-                Add);
-
-        private static readonly Parser<Expression> Exp
-            = OptionalWhitespaces.Then(Mul).ThenSkip(OptionalWhitespaces);
-        
-        */
-
         private static readonly Parser<Expression> Mul
             = Or(Num.ThenSkip(Char('*')).ThenAdd(Lazy(() => Mul)).Transform(x => new MulExpression(x.Item1, x.Item2) as Expression), Num).Trim();
 
