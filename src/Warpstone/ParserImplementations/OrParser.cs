0a0e53cbe45c7d579a9014b7cccda3dc0fb7e4e5
Wesley Baartman <9980136+CptWesley@users.noreply.github.com>
2025-06-04 10:16:27 +0300

    no message

--- src/Warpstone/ParserImplementations/OrParser.cs
+++ src/Warpstone/ParserImplementations/OrParser.cs
@@ -1,17 +1,29 @@
 namespace Warpstone.ParserImplementations;
 
+/// <summary>
+/// Represents a parser that parses either the provided <paramref name="Left"/> or <paramref name="Right"/> option.
+/// </summary>
+/// <typeparam name="T">The result type of the parsers.</typeparam>
+/// <param name="Left">The first parser to try.</param>
+/// <param name="Right">The second parser to try.</param>
 public sealed record OrParser<T>(IParser<T> Left, IParser<T> Right) : IParser<T>
 {
+    /// <summary>
+    /// The continuation parser when executing in iterative mode.
+    /// </summary>
     public Continuation Continue { get; } = new(Right);
 
+    /// <inheritdoc />
     public Type ResultType => typeof(T);
 
+    /// <inheritdoc />
     public void Apply(IIterativeParseContext context, int position)
     {
         context.ExecutionStack.Push((position, Continue));
         context.ExecutionStack.Push((position, Left));
     }
 
+    /// <inheritdoc />
     public UnsafeParseResult Apply(IRecursiveParseContext context, int position)
     {
         var left = Left.Apply(context, position);
@@ -25,10 +37,16 @@ public sealed record OrParser<T>(IParser<T> Left, IParser<T> Right) : IParser<T>
         return right;
     }
 
+    /// <summary>
+    /// The continuation parser when executing in iterative mode.
+    /// </summary>
+    /// <param name="Right">The second parser to try.</param>
     public sealed record Continuation(IParser Right) : IParser
     {
+        /// <inheritdoc />
         public Type ResultType => throw new NotSupportedException();
 
+        /// <inheritdoc />
         public void Apply(IIterativeParseContext context, int position)
         {
             var leftResult = context.ResultStack.Peek();
@@ -42,6 +60,7 @@ public sealed record OrParser<T>(IParser<T> Left, IParser<T> Right) : IParser<T>
             context.ExecutionStack.Push((position, Right));
         }
 
+        /// <inheritdoc />
         public UnsafeParseResult Apply(IRecursiveParseContext context, int position)
             => throw new NotSupportedException();
     }
--- src/Warpstone/Parsers.cs
+++ src/Warpstone/Parsers.cs
@@ -1,12 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Warpstone.Internal.Parsers;
-using Warpstone.ParserImplementations;
-using Warpstone.Parsers;
-
 namespace Warpstone;
 
 public static class Parsers
