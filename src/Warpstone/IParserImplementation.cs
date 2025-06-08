a44236c2e410f81fcb48fb2b45ae96dd42e224ad
Wesley Baartman <9980136+CptWesley@users.noreply.github.com>
2025-06-08 21:20:45 +0300

    no message

--- src/Warpstone/IParserImplementation.cs
+++ src/Warpstone/IParserImplementation.cs
@@ -19,12 +19,19 @@ public interface IParserImplementation
     /// <param name="context">The parsing context.</param>
     /// <param name="position">The position to apply it to.</param>
     public void Apply(IIterativeParseContext context, int position);
+
+    /// <summary>
+    /// The parser expression that represents this implementation.
+    /// </summary>
+    public IParser ParserExpression { get; }
 }
 
 /// <summary>
 /// Interface for all typed parser implementations.
 /// </summary>
 /// <typeparam name="T">The result type being parsed.</typeparam>
-#pragma warning disable S2326 // Unused type parameters should be removed
-public interface IParserImplementation<out T> : IParserImplementation;
-#pragma warning restore S2326 // Unused type parameters should be removed
+public interface IParserImplementation<out T> : IParserImplementation
+{
+    /// <inheritdoc cref="IParserImplementation.ParserExpression" />
+    public new IParser<T> ParserExpression { get; }
+}
